7111782cf97b5aa4e1e29038c16cc3e0f89bcd9b
jcharly <chivas_j_c@hotmail.com>
2014-10-14 12:46:54 -0500

    Agregando los archivos creados

---
+++ nuevaPagina/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="css/miestilo.css">
+	<title>Tienda</title>
+</head>
+<body>
+	<div class="container" id = "fondo">
+		<div class="col-lg-4">
+		</div>
+		<div class="col-lg-4">
+			<?php include("registro.php") ?>
+		</div>
+		<div class="col-lg-4">	
+		</div>
+
+		
+
+
+
+	</div>
+</body>
+</html>
---
+++ nuevaPagina/registro.php
@@ -0,0 +1,21 @@
+<?php 
+ ?>
+	<h3 class= "text-center">Registro</h3>
+	<form action="validarRegistro.php" method="POST" id= "registro">
+		<div class="row">
+			<label for="nombre">Nombre:</label>
+			<input type="text" id="nombre" name="nombre">
+		</div>
+		<div class="row">
+			<label for="correo">Correo:</label>
+			<input type="email" id="correo" name="correo">
+		</div>
+		<div class="row">
+			<label for="pass">Contraseña:</label>
+			<input type="password" id="pass" name="pass">
+		</div>
+		<button class= "btn btn-warning" type="submit">Registrar</button>
+	</form>
+
+ <?php 
+  ?>
